e8f9d50f090681f81ac0c6cc8e0da1d4b15f0f68
Fabian Trottmann <fabian.trottmann@bbv.ch>
2015-05-16 14:58:56 +0200

    added appccelerate production target for versioning

--- sources/OctoSample.Database/Properties/GlobalAssemblyInfo.cs
+++ sources/OctoSample.Database/Properties/GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿//-------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="Appccelerate">
+// <copyright file="GlobalAssemblyInfo.cs" company="Appccelerate">
 //   Copyright (c) 2008-2015
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,24 +22,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OctoSample.Database")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Appccelerate")]
+[assembly: AssemblyProduct("Appccelerate")]
+[assembly: AssemblyCopyright("Copyright ©  2008-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aa98ed7e-466e-474c-9a7d-fec2647396eb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: ComVisible(false)]
