5931aaf79476a3df0ffdf138594bbc203509a599
Fabian Trottmann <fabian.trottmann@bbv.ch>
2015-05-16 12:09:58 +0200

    removed scripts

--- scripts/CreateNugetPackages_WithSpecificNuspec.bat
+++
@@ -1,4 +0,0 @@
-set "version=%1%"
-set "octoPackNugetProps=/p:OctoPackNuGetProperties=suffix=release;title=My Title;version=%version%;myname=Fabian"
-c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ..\sources\OctopusSample.sln /property:Configuration=Release /p:RunOctoPack=true "%octoPackNugetProps%"
-pause
