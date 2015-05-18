14d37449a51ee56ab1ef254f27709407dcb955f1
Fabian Trottmann <fabian.trottmann@bbv.ch>
2015-05-18 15:51:41 +0200

    minor changes

--- scripts/BuildAndDeploy.bat
+++ scripts/BuildAndDeploy.bat
@@ -1,3 +1,2 @@
-set "version=%1%"
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ..\source\OctopusSample.sln /property:Configuration=Release /p:RunOctoPack=true /p:OctoPackPublishPackageToHttp=http://localhost:8090/nuget/packages /p:OctoPackPublishApiKey=API-JHSWHF2QKMKECBVLSKWLZAOV7I
 pause
